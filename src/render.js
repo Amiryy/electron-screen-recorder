28d7686616577e5a887a01d9f0a946db42708a5a
Amir <amirydev@gmail.com>
2020-12-12 21:04:18 +0200

    media recorder & file saving

--- src/render.js
+++ src/render.js
@@ -1,12 +1,21 @@
 const { desktopCapturer, remote } = require("electron");
-const { Menu } = remote;
+const { Menu, dialog } = remote;
+const { filePath } = require("fs");
 
 const videoElement = document.querySelector("video");
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
 const videoSelectBtn = document.querySelector("#videoSelectBtn");
 
-videoSelectBtn.addEventListener("click", getVideoSources);
+let mediaRecorder; // MediaRecorder instance to capture footage
+const recordedChunks = [];
+
+videoSelectBtn.onclick = getVideoSources;
+startBtn.onclick = () => {
+  startBtn.classList.add("red");
+  startBtn.classList.add("disabled")
+  startBtn.innerText = "Recording";
+};
 
 // get available video sources
 async function getVideoSources() {
@@ -37,8 +46,40 @@ async function selectSource(source) {
     }
   };
 
-  //create stream
+  // create a stream
   const stream = await navigator.mediaDevices.getUserMedia(contstrains);
-  videoElement.src = stream;
-  videoElement.play();
+  
+  // preview the source in a video element
+  videoElement.srcObject = stream;
+  videoElement.onloadedmetadata = () => videoElement.play();
+  
+  // create the Media Recorder
+  const recorderOptions = { mimeType: "video/webm; codecs=vp9" };
+  mediaRecorder = new MediaRecorder(stream, recorderOptions);
+
+  // Register Event Handlers
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.onstop = handleStop;
+}
+
+function handleDataAvailable(e) {
+  console.log("video data available");
+  recordedChunks.push(e.data);
+}
+
+async function handleStop(e) {
+  const blob = new Blob(recordedChunks, {
+    type: "video/webm; codecs=vp9"
+  });
+
+  const buffer = Buffer.from(await blob.arrayBuffer());
+
+  const { filePath } = await dialog.showSaveDialog({
+    buttonLabel: "Save Video",
+    defaultPath: `vid-${Date.now()}.webm`
+  });
+
+  console.log(filePath);
+
+  wrtieFile(filePath, buffer, () => console.log("Video Saved Successfuly"));
 }
