f83ec744f32345502df529eca25d067da4edcdac
Amir <amirydev@gmail.com>
2020-12-12 19:30:22 +0200

    video menu popup and source selection methods

--- src/render.js
+++ src/render.js
@@ -1,10 +1,44 @@
-console.log("hellooooo");
+const { desktopCapturer, remote } = require("electron");
+const { Menu } = remote;
 
 const videoElement = document.querySelector("video");
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
 const videoSelectBtn = document.querySelector("#videoSelectBtn");
 
-async function getVideoSources() {
+videoSelectBtn.addEventListener("click", getVideoSources);
 
+// get available video sources
+async function getVideoSources() {
+  const inputSources = await desktopCapturer.getSources({
+    types: ["window", "screen"]
+  });
+
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map(source => ({
+      label: source.name,
+      click: () => selectSource(source)
+    }))
+  )
+
+  videoOptionsMenu.popup();
+}
+
+async function selectSource(source) {
+  videoSelectBtn.innerText = source.name;
+  
+  const contstrains = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: "desktop",
+        chromeMediaSourceId: source.id
+      }
+    }
+  };
+
+  //create stream
+  const stream = await navigator.mediaDevices.getUserMedia(contstrains);
+  videoElement.src = stream;
+  videoElement.play();
 }
