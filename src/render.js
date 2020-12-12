b2c8666ab058050f312ab55f5156856647d1b028
Amir <amirydev@gmail.com>
2020-12-12 18:57:42 +0200

    creating render file

---
+++ src/render.js
@@ -0,0 +1,10 @@
+console.log("hellooooo");
+
+const videoElement = document.querySelector("video");
+const startBtn = document.querySelector("#startBtn");
+const stopBtn = document.querySelector("#stopBtn");
+const videoSelectBtn = document.querySelector("#videoSelectBtn");
+
+async function getVideoSources() {
+
+}
