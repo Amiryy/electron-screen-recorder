0fff831acd04f291af29b96dc2b386c66c836e6e
Amir <amirydev@gmail.com>
2020-12-12 21:23:00 +0200

    styling improvements

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 720,
+    width: 1440,
+    height: 1080,
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true
